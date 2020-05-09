cdd0ef81e4887a58b185e9bbf2196262e04d8b20
valentinashara <63387297+valentinashara@users.noreply.github.com>
2020-05-09 02:41:46 -0300

    checkpoint commit

--- checkpoint.js
+++ checkpoint.js
@@ -5,8 +5,23 @@ function menorMayor(numeros) {
   // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
   // que contenga el menor número del arreglo en la posición cero y el mayor número del arreglo en la posición 1.
   // Ej: menorMayor([4, 6, 1, 7, 15]) // retorna [1, 15]
+ 
+  var maximo = numeros[0]
+  var minimo = numeros[0]
+  for (var i = 0 ; i<numeros.length; i++) {
+    if (maximo < numeros[i]) {
+      maximo = numeros[i]
+    }
+  }
 
-}
+  for (var i = 0 ; i<numeros.length; i++) {
+    if (minimo > numeros[i]) {
+      minimo = numeros[i]
+    }
+  }
+
+  return [minimo, maximo]
+};
 
 function stringMasLarga(strings) {
   // Escribe una función llamada stringMasLarga, que tome un arreglo de strings llamado 'strings'
@@ -14,8 +29,21 @@ function stringMasLarga(strings) {
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-
-}
+  var maslarga = strings[0].length;
+  strings.forEach(function(element) {
+    if (maslarga < element.length){
+      maslarga = element.length
+    }
+    
+  });
+ 
+  for (var i = 0; i < strings.length; i++) {
+   if (strings[i].length == maslarga) {
+     return strings[i]
+   }
+  }
+ 
+};
 
 function buscarAmigo(amigos, nombre) {
   // Escribe una funcion que reciba un array de amigos (objetos) y un nombre (string).
@@ -24,8 +52,13 @@ function buscarAmigo(amigos, nombre) {
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
-  
-}
+
+  for(var i=0; i< amigos.length; i++) {
+    if (amigos[i].nombre === nombre) {
+      return amigos[i]
+    }
+  }
+};
 
 function sumArray(array, n) {
   // Escribir un algoritmo que, dada un arreglo de números ordenados y un número n, 
@@ -35,6 +68,15 @@ function sumArray(array, n) {
   // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
   // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
   // pista: podes usar bucles anidados;
+ 
+  for(var i=0; i < array.length; i++){
+  	var primerElemento = array[i];
+  	for(var j=0; j < array.length; j++){
+  		var segundoElemento = array[j];
+  		if(primerElemento!= segundoElemento && primerElemento+segundoElemento === n) return true
+  	}
+  }
+  return false;
   
 };
 
@@ -46,8 +88,7 @@ function pluck(array, propiedad) {
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
   // pista: es una buena oportunidad para usar map.
-  
-  
+  return array.map(function(elem) { return elem[propiedad]})
 }
 
 // =======================================================================
@@ -61,6 +102,10 @@ function crearClasePersona() {
 
   class Persona {
     constructor(nombre, edad, hobbies, amigos) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
       
     }
 
@@ -68,27 +113,31 @@ function crearClasePersona() {
       // el metodo addFriend recibe un string nombre y un entero edad y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // no debe retornar nada.
-      
-    }
+      var amigo = {nombre: nombre, edad: edad}
+        this.amigos.push(amigo)
+      }
+    
 
     addHobby(hobby) {
       // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
       // no debe retornar nada.
+      this.hobbies.push(hobby)
       
-    }
+    };
     getFriends() {
       // Escribe una función que retorne un arreglo con sólo los nombres del arreglo de amigos
       // de la persona.
       // Ej:
       // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
-      
-    }
+      var amiguetes = this.amigos.map(function(amigo){return amigo.nombre})
+      return amiguetes
+    };
 
     getHobbies() {
       // Escribe una función que retorne un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() // retorna ['correr', 'dormir', 'nadar']
-      
+     return this.hobbies.map(function(hobby){return hobby})
     }
 
     getPromedioEdad() {
@@ -104,6 +153,12 @@ function crearClasePersona() {
       //   }]
       // }
       // persona.getPromedioEdad() // retorna 29
+      var suma = this.amigos.reduce(function(acc, elemento){
+        acc = acc + elemento.edad
+        return acc 
+      },0)
+
+      return suma/this.amigos.length
     }
   };
 
--- teoria.md
+++ teoria.md
@@ -1,24 +1,24 @@
 ## Explica los siguentes conceptos con tus propias palabras. (no más de tres líneas por respuesta).
 
-* HTML:
+* HTML: lenguaje de marcado (esto quiere decir que cuenta con etiquetas, tags) que sirve como base para la construcción de sitios web y define su contenido. Estos tags dan información sobre la estructura del sitio.
 
-* CSS:
+* CSS: se trata de un lenguaje de diseño utilizado para dar estilo y una visual al sitio web. Puede serr aplicado a HTML. El sistema para aplicar el estilo se denomina en cascada, que define la regla prioritara a aplicar.
 
-* Git:
+* Git: es un sistema de control de versiones. En vez de guardar el archivo una y otra vez como lo haríamos cuando hacemos un cambio, Git nos permite guardar el contenido del cambio con fecha y usuario. También nos permite recuperar versiones pasadas y fusionar distintas ramas de trabajo.
 
-* Arreglos:
+* Arreglos: se trata de un tipo especial de objeto (array), una colección en lista de datos. Estos datos pueden ser de varios tipos, como números, strings, funciones, etc.
 
-* Objetos:
+* Objetos: son elementos que contienen uno o varios pares de propiedad:valor. El valor asociado también puede tratarse de una función.
 
-* Variables:
+* Variables: son valores guardados, una representación de un lugar en la memoria para acceder a un dato. El valor de la variable puede ir cambiando a lo largo del código.
 
-* Métodos:
+* Métodos: se trata de funciónes que forman parte de un valor asignado a una propiedad de un objeto.
 
-* DOM:
+* DOM: es una interfaz que interpreta HTML estableciendo como acceder a los elementos y manipularlos. También permite acceder a JavaScript.
 
-* Atributo de un tag HTML:
+* Atributo de un tag HTML: son valores que se pueden o no asignar a un tag HTML.  Permite agregar configuraciones adicionales al tag, como por ejemplo el atributo style, class o id.
 
-* Selector CSS:
+* Selector CSS: es la forma que posee CSS para referirse a algún elemento de HTML, lo identifica para poder aplicarle la declaración, es decir la propiedad que le dará estilo dentro de CSS. Puede ser de etiqueta (por ejemplo: div, nav, p), de id o de clase.
 
-* Un función constructora:
+* Un función constructora: se denominan así a las clases, una función que se utiliza como plantilla o modelo para crear objetos que comparten propiedades. Por ejemplo, una funcion contructora podría ser para crear usuarios (comparten propiedades).
 
